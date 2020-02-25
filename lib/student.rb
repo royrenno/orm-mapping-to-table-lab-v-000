c04ae8af3107d22b7e73bfcb66ce6f42dd37ca02
roy renno <royrenno@yahoo.com>
2020-02-25 06:23:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,18 @@ class Student
   @grade = grade
   end
 
+  def self.create_table
+    sql= <<-SQL
+    CREATE TABLE IF NOT EXITS students(
+    id INTERGER PRIMARY KEYm
+    name TEXT
+    grade TEXT
+    )
+    SQL
+    DB[:conn].excute(sql)
+  end
+  
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
