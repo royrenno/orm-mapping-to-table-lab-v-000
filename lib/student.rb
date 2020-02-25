efcec11a028244c5a73ec9da55369ffc81ce2642
roy renno <royrenno@yahoo.com>
2020-02-25 06:35:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,11 +25,9 @@ class Student
       SQL
       DB[:conn].execute(sql)
     end
-    
+
 end
 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
-
