7070059d6e7a1c093db58a44925638301000a3f5
roy renno <royrenno@yahoo.com>
2020-02-25 07:01:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,54 +1,48 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name,:grade
-  attr_reader :id
+  attr_accessor :name, :grade
+ attr_reader :id
 
-  def initialize(name,grade,id = nil)
-  @name = name
-  @grade = grade
-  @id = id
-  end
+ def initialize (name, grade, id = nil)
+   @name = name
+   @grade = grade
+   @id = id
+ end
 
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students(
-    id INTERGER PRIMARY KEY,
-    name TEXT,
-    grade INTERGER
-    )
+ def self.create_table
+   sql = <<-SQL
+     CREATE TABLE IF NOT EXISTS students (
+       id INTEGER PRIMARY KEY, 
+       name TEXT,
+       grade INTEGER
+       )
+   SQL
+     DB[:conn].execute(sql)
+ end
 
-    SQL
+ def self.drop_table
+   sql = <<-SQL
+     DROP TABLE students
+   SQL
 
-       DB[:conn].execute(sql)
-  end
+   DB[:conn].execute(sql)  
+ end
 
-  def self.drop_table
-    sql =<<-SQL
-      DROP TABLE students
-      SQL
+ def save
+   sql = <<-SQL
+     INSERT INTO students (name, grade)
+       VALUES (?, ?)
+   SQL
 
-        DB[:conn].execute(sql)
-    end
+   DB[:conn].execute(sql, self.name, self.grade)
+ end
 
-    def save
-      sql =<<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.grade)
-    end
-
-    def self.create(name:, grade:)
-      student = Student.new(name, grade)
-      student.save
-      student
-    end
-
-end
+ def self.create(name:, grade:)
+   student = Student.new(name, grade)
+   student.save
+   student
+ end
 
 
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+end	
